489c31cfd493c71852de766776d04a57857dbd57
JirapunJunruk <jirapun0907@gmail.com>
2023-08-11 15:09:34 +0700

    func-components
    func-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import './style.css'
+import { Content, Footer, Header } from './func-components';
 
 function App() {
-  
-  
+
   return (
     <>
-    <h2>Developerthai.com</h2>
-    <div className='title'>React & Reat name</div>
-
+    <Header/>
+    <Content/>
+    <Footer/>
     </>
   )
-   
 }
-
 export default App;
