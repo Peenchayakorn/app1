bb80d3e3e52021a6a8585dec1ae6e520275831d1
JirapunJunruk <jirapun0907@gmail.com>
2023-08-18 15:19:15 +0700

    event-data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Content, Footer, Header } from './func-components';
 import Calendar from './class-components';
 import Banner from './banner';
 import {Calculator} from './calculator';
+import { EventData2 } from './event-data';
 
 function App() {
   return (
-   <Calculator/>
+   <EventData2/>
   )
 }
 export default App;
