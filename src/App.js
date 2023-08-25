19b84b05140d6af81c5f283a70185d0139e4e362
JirapunJunruk <jirapun0907@gmail.com>
2023-08-25 15:22:44 +0700

    refs-array and state-func

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Banner from './banner';
 import {Calculator} from './calculator';
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 
 function App() {
   return (
-   <RefsFunc/>
+   <MessageBox/>
   )
 }
 export default App;
