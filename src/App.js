541dfdf3826b1584b584472f55c4b19b50a04097
Peenchayakorn <111230250+Peenchayakorn@users.noreply.github.com>
2023-09-08 15:25:28 +0700

    Signin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Banner from './banner';
-import {Calculator} from './calculator';
-import { EventData2 } from './event-data';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
-
-
-function App() {
+import React from 'react'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+  export default function App() {
+  let [user, setUser] = React.useState()
   return (
-   <MessageBox/>
+    <userContext.Provider value={[user, setUser]}>
+    <Header2/>
+    <Content2/>
+    </userContext.Provider>
   )
-}
-export default App;
+  }
