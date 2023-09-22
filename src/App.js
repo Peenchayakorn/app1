1d988724098f63351a0e4571dfa76714ebd5d908
Peenchayakorn <111230250+Peenchayakorn@users.noreply.github.com>
2023-09-22 23:27:28 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { userContext } from './context'
-import Header2 from './context-header2'
-import Content2 from './context-content2'
-  export default function App() {
-  let [user, setUser] = React.useState()
+import React from 'react';
+//import { Content, Footer, Header } from './func-components';
+//import Calendar from './class-components';
+//import Banner from './banner';
+//import {Calculator} from './calculator';
+//import { EventData2 } from './event-data';
+//import RefsFunc from './refs-func';
+//import RefsArray from './refs-array';
+//import MessageBox from './state-func';
+import Router2 from './router2';
+import Router3 from './router3';
+//import Router3 from './router3';
+
+
+
+export default function App() {
   return (
-    <userContext.Provider value={[user, setUser]}>
-    <Header2/>
-    <Content2/>
-    </userContext.Provider>
+    <Router3/>
+    
   )
-  }
+}
