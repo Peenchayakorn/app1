2b5c5f89d88662547626f7c9ee29acf3f550ae18
Peenchayakorn <111230250+Peenchayakorn@users.noreply.github.com>
2023-09-29 15:49:43 +0700

    form text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-//import { Content, Footer, Header } from './func-components';
-//import Calendar from './class-components';
-//import Banner from './banner';
-//import {Calculator} from './calculator';
-//import { EventData2 } from './event-data';
-//import RefsFunc from './refs-func';
-//import RefsArray from './refs-array';
-//import MessageBox from './state-func';
-import Router2 from './router2';
-import Router3 from './router3';
-//import Router3 from './router3';
-
-
-
+import React from 'react'
+import FormCheck from './form-check';
 export default function App() {
-  return (
-    <Router3/>
+return <FormCheck/>
+
     
-  )
 }
---
+++ src/form-check.js
@@ -0,0 +1,110 @@
+import React from 'react';
+
+export default function FormCheck() {
+  const switchReq = React.useRef();
+  const styles = ['bold', 'italic', 'underline'];
+  const sizes = ['small', 'medium', 'large', 'larger'];
+  let checkedStyles = [];
+  let checkedSize = '';
+
+  const OnChangeCheck = (event) => {
+    let checked = event.target.checked;
+    let value = event.target.value;
+    let idx = checkedStyles.indexOf(value);
+
+    if (checked && idx === -1) {
+      checkedStyles.push(value);
+    } else {
+      checkedStyles.splice(idx, 1);
+    }
+  };
+
+  const onChangeRadio = (event) => {
+    checkedSize = event.target.value;
+  };
+
+  const onClickButton = () => {
+    if (switchReq.current.checked) {
+      if (checkedStyles.length === 0) {
+        alert('Please select style');
+        return;
+      } else if (checkedSize === '') {
+        alert('Please Select Size');
+        return;
+      }
+    }
+    let msg = 'selected style(s): ' + checkedStyles.join(', ');
+    msg += '\nselected size: ' + checkedSize;
+    alert(msg);
+  };
+
+  return (
+    <div className="">
+      <div
+        className="mt-4 mx-auto p-3 rounded"
+        style={{ width: '450px', background: '#cee' }}
+      >
+        <form className="mx-auto">
+          <span>font style:</span>&nbsp;&nbsp;
+          {styles.map((st, i) => {
+            return (
+              <div className="form-check form-check-inline mb-2" key={i}>
+                <input
+                  type="checkbox"
+                  id={'check' + i}
+                  value={st}
+                  className="form-check-input"
+                  onChange={OnChangeCheck}
+                />
+                <label htmlFor={'check' + i} className="form-check-label">
+                  {st}
+                </label>
+              </div>
+            );
+          })}
+          <br />
+          <span>font size:</span>&nbsp;&nbsp;
+          {sizes.map((sz, i) => {
+            return (
+              <div className="form-check form-check-inline mb-2" key={i}>
+                <input
+                  type="checkbox"
+                  id={'radio' + i}
+                  name="fontSize"
+                  value={sz}
+                  className="form-check-input"
+                  onChange={OnChangeCheck}
+                />
+                <label htmlFor={'radio' + i} className="form-check-label">
+                  {sz}
+                </label>
+              </div>
+            );
+          })}
+          <div className="form-check form-switch mt-3">
+            <input
+              type="checkbox"
+              id="sw"
+              value="require"
+              className="form-check-input"
+              ref={switchReq}
+            />
+            <label htmlFor="sw" className="form-check-label">
+              require
+            </label>
+          </div>
+
+          <div className="text-center mt-4">
+            <button
+              type="button"
+              className="btn btn-sm px-4 btn-primary"
+              onClick={onClickButton}
+            >
+              OK
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
